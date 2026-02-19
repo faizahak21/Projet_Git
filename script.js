bba4af47e532f7aa587ba059652e6ad30330512e
hakem faiza <hakemfaiza21@gmail.com>
2026-02-19 16:30:13 +0100

    feat: add person object and day-of-week display

--- script.js
+++ script.js
@@ -4,3 +4,41 @@ let b = 45;
 
 b += a;
 console.log("resultat de a + b = " + b);
+
+let aPerson = { 
+    name: 'Jean',
+    age: 35,
+    gender: 'masculin',
+    interest: ['musique', 'Jeux videos'],
+  };
+  
+  aPerson.sayHello = function () {
+    console.log('Bonjour ! Je suis ' + this.name + '.');
+  };
+  
+  aPerson.sayHello();
+  
+  let day;
+switch (new Date().getDay()) {
+  case 0:
+    day = "Dimanche";
+    break;
+  case 1:
+    day = "Lundi";
+    break;
+  case 2:
+    day = "Mardi";
+    break;
+  case 3:
+    day = "Mercredi";
+    break;
+  case 4:
+    day = "Jeudi";
+    break;
+  case 5:
+    day = "Vendredi";
+    break;
+  case 6:
+    day = "Samedi";
+}
+console.log("Aujourd'hui nous sommes " + day);
